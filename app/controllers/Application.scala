3d23698279bc21ea9c53ae9112ca225dcfe56db4
wongcy6 <rcwong@outlook.com>
2014-05-11 21:33:46 -0700

    Add basic logic

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,19 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("请输入验证码", true))
+  }
+
+  def update = Action { request =>
+    if (1==2) {
+      Ok(views.html.index("验证成功", true))
+    } else {
+      if (1==2) {
+        Ok(views.html.index("验证失败. 验证码不存在", false))
+      } else {
+        Ok(views.html.index("验证失败。验证吗已使用", false))
+      }
+    }
   }
 
 }
